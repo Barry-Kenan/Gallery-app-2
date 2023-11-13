447a197c59b3c57c020aa94e483397a50e51d060
Бердыев Бары <bary.berdiyev.97@mail.ru>
2023-11-13 14:52:50 +0700

    кнопка закрытия карточки

--- src/entities/Card/ui/Card.tsx
+++ src/entities/Card/ui/Card.tsx
@@ -1,13 +1,16 @@
-import { getImageName } from '@/shared/lib';
+import { getImageDate, getImageName, getImageSize } from '@/shared/lib';
 import { FC, memo } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import styles from './Card.module.scss';
 import { CardProps } from './Card.props';
+import CloseIcon from './icons/close.svg';
 
 export const Card: FC<CardProps> = memo(({ card }) => {
 	const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 	const name = getImageName(card);
+	const date = getImageDate(card);
+	const size = getImageSize(card);
 
 	return (
 		<div className={styles.card}>
@@ -17,8 +20,13 @@ export const Card: FC<CardProps> = memo(({ card }) => {
 				effect='blur'
 				className={styles.image}
 			/>
+			<button className={styles.closeButton}>
+				<CloseIcon />
+			</button>
 			<div className={styles.content}>
 				<h5>{name}</h5>
+				<h5>{date}</h5>
+				<h5>{size}</h5>
 			</div>
 		</div>
 	);
