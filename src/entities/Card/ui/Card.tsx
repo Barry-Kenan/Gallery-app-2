d72f547e786d5abc8bbcff11488be5a42d36786d
Бердыев Бары <bary.berdiyev.97@mail.ru>
2023-11-14 10:03:08 +0700

    удаление карточек

--- src/entities/Card/ui/Card.tsx
+++ src/entities/Card/ui/Card.tsx
@@ -1,4 +1,5 @@
 import { getImageDate, getImageName, getImageSize } from '@/shared/lib';
+import { useImagesStore } from '@/shared/model';
 import { FC, memo } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import styles from './Card.module.scss';
@@ -6,12 +7,16 @@ import { CardProps } from './Card.props';
 import CloseIcon from './icons/close.svg';
 
 export const Card: FC<CardProps> = memo(({ card }) => {
+	const { setDeletedImage } = useImagesStore();
 	const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 	const name = getImageName(card);
 	const date = getImageDate(card);
 	const size = getImageSize(card);
 
+	const handleClick = () => {
+		setDeletedImage(card);
+	};
 	return (
 		<div className={styles.card}>
 			<LazyLoadImage
@@ -20,7 +25,7 @@ export const Card: FC<CardProps> = memo(({ card }) => {
 				effect='blur'
 				className={styles.image}
 			/>
-			<button className={styles.closeButton}>
+			<button className={styles.closeButton} onClick={handleClick}>
 				<CloseIcon />
 			</button>
 			<div className={styles.content}>
--- src/features/Cards/ui/Cards.tsx
+++ src/features/Cards/ui/Cards.tsx
@@ -4,13 +4,14 @@ import { FC, useDeferredValue } from 'react';
 import styles from './Cards.module.scss';
 
 export const Cards: FC = () => {
-	const { images } = useImagesStore();
-	const deferredImages = useDeferredValue(images);
+	const { images, deletedImages } = useImagesStore();
+	const filteredImages = images.filter(e => !deletedImages.includes(e.image));
+	const deferredImages = useDeferredValue(filteredImages);
 
 	return (
 		<div className={styles.grid}>
 			{deferredImages?.map(e => (
-				<Card key={e.id} card={e} />
+				<Card key={e.image} card={e} />
 			))}
 		</div>
 	);
